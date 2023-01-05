a2474b0983ec1292a5ccad92c06d3c0f122a132d
Kelly F <95229425+kellyraefoote@users.noreply.github.com>
2023-01-04 18:08:24 -0800

    mid exercise 1

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+    const usersRequest = await fetch (
+        "https://randomuser.me/api?results=5"
+    )
+    const data = await usersRequest.json();
+console.log(data);
+}
+getData();
